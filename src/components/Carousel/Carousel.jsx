8e42777267be7b8c1e62bdc432e02e6dede6fb8b
ranjanpriti <sharmavishal49313@gmail.com>
2026-05-30 13:05:01 +0530

    Update Carousel.jsx

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -42,6 +42,16 @@ function Carousel({ data, renderComponent, component }) {
         modules={[Navigation]}
         spaceBetween={10}
         slidesPerView={"auto"}
+        slidesPerGroup={1}
+        observer
+        observeParents
+        watchSlidesProgress
+        breakpoints={{
+          320: { slidesPerView: 1.2, spaceBetween: 10 },
+          640: { slidesPerView: 2.2, spaceBetween: 10 },
+          900: { slidesPerView: 3.2, spaceBetween: 10 },
+          1200: { slidesPerView: 4.2, spaceBetween: 10 },
+        }}
         style={{ padding: "0px 20px" }}
         allowTouchMove
       >
